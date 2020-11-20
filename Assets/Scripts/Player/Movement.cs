502f6741823275f7094c02f3a69041b6ff7bcd70
Chris <chrisrkemp@hotmail.co.uk>
2020-11-20 20:48:23 +0000

    Added placeholder character with horizontal movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    //Public properties
+
+
+    //Private data
+    Rigidbody2D m_rigidBody;
+    Vector2 m_movementInput;
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidBody.velocity = new Vector2(m_movementInput.x * 4, m_rigidBody.velocity.y);
+    }
+
+    private void OnMove(InputValue move)
+    {
+        Debug.Log("Test");
+        var vec = (Vector2)move.Get();
+        m_movementInput = vec;
+    }
+}
