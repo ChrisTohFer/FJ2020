a46ceb36f12c2041d2b870b9dad17166529553ef
Chris <chrisrkemp@hotmail.co.uk>
2020-11-20 22:56:03 +0000

    Added rubber band

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,26 +6,95 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     //Public properties
+    public float coyoteTime = 0.2f;
 
+    //Public references
+    public Rigidbody2D m_rigidBody;
+    public Collider2D m_collider;
+    public RubberBand m_band;
 
     //Private data
-    Rigidbody2D m_rigidBody;
     Vector2 m_movementInput;
-
-    private void Awake()
-    {
-        m_rigidBody = GetComponent<Rigidbody2D>();
-    }
+    bool m_grounded = false;
 
     private void FixedUpdate()
     {
-        m_rigidBody.velocity = new Vector2(m_movementInput.x * 4, m_rigidBody.velocity.y);
+        ApplyPlayerMovement();
     }
+
+    //Functions
+
+    void ApplyPlayerMovement()
+    {
+        CheckGrounded();
+        if(m_grounded)
+            m_rigidBody.velocity = new Vector2(m_movementInput.x * 4, m_rigidBody.velocity.y);
+    }
+
+    void CheckGrounded()
+    {
+        Vector2 rayStart = transform.position + Vector3.down * (m_collider.bounds.extents.y + 0.1f);
+        //Debug.DrawRay(rayStart, Vector3.down * 0.1f, Color.red);
+
+        RaycastHit2D result;
+        result = Physics2D.Raycast(rayStart, Vector2.down, 0.1f);
+        if (result.transform != null)
+        {
+            if (result.transform.tag == "Ground")
+            {
+                m_grounded = true;
+                StopCoroutine("CoyoteWait");
+            }
+        }
+        else if (m_grounded)
+            StartCoroutine(CoyoteWait(coyoteTime));
+    }
+
+    //Coroutines
+
+    IEnumerator CoyoteWait(float duration)
+    {
+        for (float time = 0; time < duration; time += Time.fixedDeltaTime)
+        {
+            Debug.Log(time + ", " + duration);
+            yield return new WaitForFixedUpdate();
+        }
+        m_grounded = false;
+    }
+
+    //Callbacks
 
     private void OnMove(InputValue move)
     {
-        Debug.Log("Test");
         var vec = (Vector2)move.Get();
         m_movementInput = vec;
     }
+
+    private void OnClick(InputValue button)
+    {
+        if (button.isPressed)
+        {
+            var position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            position.z = 0;
+            m_band.PinToLocation(position);
+        }
+        else
+            m_band.Unpin();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.otherCollider.tag == "Ground")
+        {
+            m_grounded = true;
+            StopCoroutine("CoyoteWait");
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.otherCollider.tag == "Ground")
+            StartCoroutine(CoyoteWait(coyoteTime));
+    }
+
+
 }
---
+++ Assets/Scripts/Player/RubberBand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubberBand : MonoBehaviour
+{
+    //Public Properties
+    public float naturalLength = 1f;
+    public float thinningPower = 1f;
+    public Transform pin;
+    public Rigidbody2D rigidBody;
+
+    private void Update()
+    {
+        var difference = pin.position - transform.position;
+        var angle = -Vector2.SignedAngle(difference, Vector2.right);
+
+        transform.eulerAngles = new Vector3(0f, 0f, angle);
+        transform.localScale = new Vector3(difference.magnitude, 1f / Mathf.Pow(difference.magnitude, thinningPower));
+    }
+
+    public void PinToLocation(Vector3 pos)
+    {
+        pin.position = pos;
+        rigidBody.simulated = false;
+    }
+    public void Unpin()
+    {
+        rigidBody.simulated = true;
+    }
+
+}
