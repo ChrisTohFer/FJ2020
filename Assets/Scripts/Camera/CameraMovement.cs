1995407e167c2e906580d753971c9267a09e5c71
Chris <chrisrkemp@hotmail.co.uk>
2020-11-21 13:18:46 +0000

    Added camera points of interest

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -8,22 +8,45 @@ public class CameraMovement : MonoBehaviour
     public float minSpeed = 1f;
     public float speedIncreasePerDistance = 1f;
     public Transform playerTransform;
-    //public List<>
+    List<CameraPointOfInterest> pointsOfInterest = new List<CameraPointOfInterest>();
 
     private void FixedUpdate()
     {
-        var player2DPosition = (Vector2)playerTransform.position;
-        var camera2DPosition = (Vector2)Camera.main.transform.position;
-        var difference = player2DPosition - camera2DPosition;
+        var targetPos = CalculateCameraTarget();
+        var cameraPos= (Vector3)(Vector2)Camera.main.transform.position;
+        var difference = targetPos - cameraPos;
         var speedTick = (minSpeed + speedIncreasePerDistance * difference.magnitude) * Time.fixedDeltaTime;
 
         if (speedTick > difference.magnitude)
-            Camera.main.transform.position = playerTransform.position + Vector3.back * 10f;
+            Camera.main.transform.position = targetPos + Vector3.back * 10f;
         else
         {
             var velocityTick = (Vector3)difference.normalized * speedTick;
             Camera.main.transform.position += velocityTick;
         }
+    }
+
+    public Vector3 CalculateCameraTarget()
+    {
+        var target = playerTransform.position;
+        var totalWeight = 1f;
+
+        foreach(CameraPointOfInterest poi in pointsOfInterest)
+        {
+            if(poi.PlayerWithinCatchmentArea())
+            {
+                target += poi.PointOfInterest() * poi.weighting;
+                totalWeight += poi.weighting;
+            }
+        }
+
+        target /= totalWeight;
+        return target;
+    }
+
+    public void AddPointOfInterest(CameraPointOfInterest poi)
+    {
+        pointsOfInterest.Add(poi);
     }
 
 }
---
+++ Assets/Scripts/Camera/CameraPointOfInterest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPointOfInterest : MonoBehaviour
+{
+    public enum Type
+    {
+        Point,
+        Line
+    }
+
+    //Public
+    public Type type = Type.Point;
+    public float weighting = 1f;
+    public float radius = 10f;
+    public Transform secondPin;
+
+    Transform playerTransform;
+
+    private void Start()
+    {
+        var moveScript = Camera.main.GetComponent<CameraMovement>();
+        moveScript.AddPointOfInterest(this);
+        playerTransform = moveScript.playerTransform;
+    }
+
+    public bool PlayerWithinCatchmentArea()
+    {
+        var playerPos = playerTransform.position;
+        switch(type)
+        {
+            case Type.Point:
+                return (playerPos - transform.position).magnitude < radius;
+            case Type.Line:
+                var line = secondPin.position - transform.position;
+                if ((Vector3.Dot(line, playerPos - transform.position) < 0))       //Camera is before point 1
+                    return false;
+                if ((Vector3.Dot(line, secondPin.position - playerPos) < 0))       //Camera is past point 2
+                    return false;
+                return Vector3.Cross(line.normalized, playerPos - transform.position).magnitude < radius;  //Camera within line radius
+            default:
+                return false;
+        }
+    }
+
+    public Vector3 PointOfInterest()
+    {
+        var playerPos = playerTransform.position;
+        switch (type)
+        {
+            case Type.Point:
+                return transform.position;
+            case Type.Line:
+                var line = secondPin.position - transform.position;
+                var perpendicularLine = playerPos - Vector3.Cross(line.normalized, playerPos - transform.position);
+                Debug.DrawRay(playerPos, Vector3.Cross(perpendicularLine, line.normalized));
+                return playerPos + Vector3.Cross(perpendicularLine, line.normalized);
+            default:
+                return transform.position;
+        }
+
+    }
+}
