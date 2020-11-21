f1bee4d4e71d692ed373f4befb5ab86c10459222
DJ-Jam1 <74789909+DJ-Jam1@users.noreply.github.com>
2020-11-21 15:36:31 +0100

    Checkpoint and Sprite Pixel Changes
    Submitting the checkpoint and set pixel scale

---
+++ Assets/Scripts/ChecpointController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChecpointController : MonoBehaviour
+{
+    public Sprite redFlag;
+    public Sprite greenFlag;
+    private SpriteRenderer checkpointSpriteRenderer;
+    public bool checkpointReached;
+    public Vector3 currentrespawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag =="Player")
+        {
+            checkpointSpriteRenderer.sprite = greenFlag;
+            checkpointReached = true;
+            currentrespawnPoint = other.transform.position;
+        }
+    }
+}
