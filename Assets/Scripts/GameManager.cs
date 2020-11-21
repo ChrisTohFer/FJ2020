2775159f946118046df2afaa487d80e5b390936a
DJ-Jam1 <74789909+DJ-Jam1@users.noreply.github.com>
2020-11-21 21:27:18 +0100

    Game Manager Implementation
    Highscore tracking stopwatch counter

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+    public Text highScore;
+    public bool LevelComplete = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+        highScore.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart += Time.deltaTime;
+        textBox.text = timeStart.ToString("F2");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            // set game state to done 
+            LevelComplete = true;
+
+            // store our time 
+            if (timeStart < PlayerPrefs.GetFloat("HighScore", 0))
+            {
+                PlayerPrefs.SetFloat("HighScore", timeStart);
+            }
+            
+
+            // Load the main menu
+
+
+        }
+    }
+}
+
