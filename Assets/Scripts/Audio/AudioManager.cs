b0a1d084637aa4fdf373db28bed89b0a90e8f8e9
Chris <chrisrkemp@hotmail.co.uk>
2020-11-22 14:52:17 +0000

    Added audiomanager script and prefab

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //Types
+    [System.Serializable]
+    public struct NamedSoundEffect
+    {
+        public string name;
+        public SFXEvent soundEffect;
+    }
+
+    //Static
+    static AudioManager singleton;
+
+    //Public
+    public SFXEvent music;
+    public NamedSoundEffect[] soundEffects;
+
+    //Private
+    Dictionary<string, SFXEvent> m_soundEffects;
+
+    private void Awake()
+    {
+        if(singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+
+            foreach(NamedSoundEffect nse in soundEffects)
+            {
+                m_soundEffects.Add(nse.name, nse.soundEffect);
+            }
+        }
+        else
+        {
+            //We already have an audio manager
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        if (singleton == this)
+            music.Play();
+    }
+
+    public static void PlaySoundEffect(string name)
+    {
+        singleton.m_soundEffects[name].Play();
+    }
+    public static void StopSoundEffect(string name)
+    {
+        singleton.m_soundEffects[name].Stop();
+    }
+
+}
