895245de7fe1b8fc2bef2850dea91e46de00129e
Chris <chrisrkemp@hotmail.co.uk>
2020-11-22 17:23:55 +0000

    Added menu scene

---
+++ Assets/Scripts/BasicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BasicTrigger : MonoBehaviour
+{
+    public UnityEvent triggered;
+    public string requiredTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(requiredTag == "" || collision.gameObject.tag == requiredTag)
+        {
+            triggered.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/MenuSceneChange.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneChange : MonoBehaviour
+{
+    public string levelOne;
+    public string slingTower;
+
+    bool changing = false;
+
+    public void LoadLevelOne()
+    {
+        if (changing)
+            return;
+        changing = true;
+
+        ScreenBlind.Enter();
+        Invoke("LoadLevelOnePrivate", 1f);
+    }
+    void LoadLevelOnePrivate()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+
+    public void LoadSlingTower()
+    {
+        if (changing)
+            return;
+        changing = true;
+
+        ScreenBlind.Enter();
+        Invoke("LoadSlingTowerPrivate", 1f);
+    }
+    void LoadSlingTowerPrivate()
+    {
+        SceneManager.LoadScene(slingTower);
+    }
+
+    public void Exit()
+    {
+        if (changing)
+            return;
+        changing = true;
+
+        ScreenBlind.Enter();
+        Invoke("ExitPrivate", 1f);
+    }
+    private void ExitPrivate()
+    {
+        Application.Quit();
+    }
+}
