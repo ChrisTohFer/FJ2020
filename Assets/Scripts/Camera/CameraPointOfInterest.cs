71bfcfb5f6ab759b17ae2d1b992d212ed31b3fe1
Chris <chrisrkemp@hotmail.co.uk>
2020-11-22 19:11:12 +0000

    Added parallax and square point of interest

--- Assets/Scripts/Camera/CameraPointOfInterest.cs
+++ Assets/Scripts/Camera/CameraPointOfInterest.cs
@@ -7,7 +7,8 @@ public class CameraPointOfInterest : MonoBehaviour
     public enum Type
     {
         Point,
-        Line
+        Line,
+        Square
     }
 
     //Public
@@ -39,6 +40,10 @@ public class CameraPointOfInterest : MonoBehaviour
                 if ((Vector3.Dot(line, secondPin.position - playerPos) < 0))       //Camera is past point 2
                     return false;
                 return Vector3.Cross(line.normalized, playerPos - transform.position).magnitude < radius;  //Camera within line radius
+            case Type.Square:
+                var check1 = playerPos.x > transform.position.x && playerPos.x < secondPin.position.x;
+                var check2 = playerPos.y < transform.position.y && playerPos.y > secondPin.position.y;
+                return check1 && check2;
             default:
                 return false;
         }
@@ -56,6 +61,8 @@ public class CameraPointOfInterest : MonoBehaviour
                 var perpendicularLine = playerPos - Vector3.Cross(line.normalized, playerPos - transform.position);
                 Debug.DrawRay(playerPos, Vector3.Cross(perpendicularLine, line.normalized));
                 return playerPos + Vector3.Cross(perpendicularLine, line.normalized);
+            case Type.Square:
+                return (transform.position + secondPin.position) / 2f;
             default:
                 return transform.position;
         }
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    //Public
+    public float parallax = .5f;
+
+    Vector3 initialPosition;
+    Vector3 initialCameraPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        initialCameraPosition = Camera.main.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = initialPosition + parallax * (Camera.main.transform.position - initialCameraPosition);
+    }
+
+}
