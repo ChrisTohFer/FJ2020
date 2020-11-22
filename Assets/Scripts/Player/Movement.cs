b83ba0e34c49294062f6f4d78c067dee0d225738
Chris <chrisrkemp@hotmail.co.uk>
2020-11-22 19:57:15 +0000

    Scene update, added swing break when colliding with terrain

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -246,6 +246,8 @@ public class Movement : MonoBehaviour
         if (angle > 0 && angle < Mathf.PI)
             phase = Mathf.PI - phase;
 
+        int failedFrames = 0; //Number of frames in a row that we didn't get close (within 1.5) to the target
+
         m_swingCoroutineActive = true;
 
         Vector3 targetPosition;
@@ -258,10 +260,16 @@ public class Movement : MonoBehaviour
             targetPosition = pivot + new Vector3(Mathf.Sin(targetAngle), -Mathf.Cos(targetAngle), 0f) * swingRadius;
             var displacement = targetPosition - transform.position;
             m_rigidBody.velocity = displacement * 5;
+            if (displacement.magnitude > 1f)
+                ++failedFrames;
+            else
+                failedFrames = 0;
             yield return new WaitForFixedUpdate();
+            if (failedFrames >= 20)
+                m_swinging = false;
         } while (m_swinging);
 
-        if (Vector3.Distance(targetPosition, transform.position) < 1.5f)
+        if (Vector3.Distance(targetPosition, transform.position) < 1.5f && failedFrames < 20)
         {
             if (phase <= Mathf.PI / 2f && phase > Mathf.PI / 2f - swingTolerance)
             {
