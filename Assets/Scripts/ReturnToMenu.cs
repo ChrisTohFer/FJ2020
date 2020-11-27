211d08175dbc655a65a0ee7f9447c5c89b00ad34
Chris <chrisrkemp@hotmail.co.uk>
2020-11-27 13:24:54 +0000

    Added ability to return to menu with escape

---
+++ Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    bool exiting = false;
+    private void Update()
+    {
+        if(!exiting && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            exiting = true;
+            ScreenBlind.Enter();
+            Invoke("LoadMenu", 1f);
+        }
+    }
+
+    private void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
